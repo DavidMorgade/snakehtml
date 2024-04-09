6839863e03b0696f38fc9d36e6f3d71f2262124b
DavidMorgade <davidmorgadegil@gmail.com>
2024-04-09 22:11:05 +0200

    updated player class, showing current score, temporal prompt to get the player name

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,30 @@
 import { Point } from "./point";
 import { Snake } from "./snake";
+import { Player } from "./player";
 
+const player = new Player("");
 const snake = new Snake();
 const point = new Point();
 const container = document.getElementById("game") as HTMLDivElement;
+const resetButton = document.getElementById(
+  "reset-button"
+) as HTMLButtonElement;
+
+resetButton.addEventListener("click", () => {
+  document.location.reload();
+});
 
 document.addEventListener("DOMContentLoaded", () => {
+  setHTMLNameAndScore();
   startGame();
 });
+
+const setHTMLNameAndScore = () => {
+  const playerName = prompt("Introduce tu nombre de jugador");
+  player.setName = playerName || "Player";
+  player.setHTMLPlayerName();
+  player.setHTMLPlayerScore();
+};
 
 const startGame = () => {
   point.generatePointOnGameBoard(container);
@@ -64,6 +81,7 @@ function updateSnakePosition() {
     point.generatePointOnGameBoard(container);
   } else {
     snake.grow();
+    player.increaseScore();
     // Generar un nuevo punto si el anterior ha sido capturado
     point.generateNewPoint();
   }
--- src/ts/player.ts
+++ src/ts/player.ts
@@ -1,9 +1,9 @@
-class Player {
+export class Player {
   private name: string;
   private score: number;
 
   constructor(name: string) {
-    this.name = name;
+    this.name = name || "Player";
     this.score = 0;
   }
 
@@ -18,5 +18,23 @@ class Player {
   }
   set setScore(score: number) {
     this.score = score;
+  }
+
+  public increaseScore(): void {
+    this.score += 1;
+    this.setHTMLPlayerScore();
+  }
+
+  public setHTMLPlayerName(): void {
+    const playerName = document.getElementById(
+      "player-name"
+    ) as HTMLHeadingElement;
+    playerName.innerHTML = this.name;
+  }
+  public setHTMLPlayerScore(): void {
+    const playerScore = document.getElementById(
+      "player-score"
+    ) as HTMLHeadingElement;
+    playerScore.innerHTML = this.score.toString();
   }
 }
